a166e76b6faf88c632c3dc0b4b4fe20902a4cbf3
AHUKV <ahu2032@gmail.com>
2025-08-07 23:45:59 +0800

    Delete scripts/gc throughput directory

--- scripts/gc throughput/run.sh
+++
@@ -1,7 +0,0 @@
-ulimit -n 999999
-
-
-/home/ubuntu/My/ParaKV-GC/cmake-build-debug/db_bench --benchmarks=fillrandom --num=100000000 --value_size=1024 --db=/media/test --vlog=/media/test &> 1024.txt
-
-/home/ubuntu/My/ParaKV-GC/cmake-build-debug/db_bench --benchmarks=fillrandom --num=800000000 --value_size=128 --db=/media/test --vlog=/media/test --vlog_size=33554432 &> 128.txt
-
