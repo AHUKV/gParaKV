63abb57180c2f982244253f4571fa89071e6dcd3
AHUKV <ahu2032@gmail.com>
2025-09-07 12:16:24 +0800

    Update ycsbc.sh

--- scripts/ycsbc.sh
+++ scripts/ycsbc.sh
@@ -47,9 +47,9 @@ OUT_FILE=${OUT_FILE:-a.txt}
 # Binary selection
 ##########################
 if [[ "$ENABLE_GC" -eq 1 ]]; then
-  YCSB_BIN="$HOME/ParaKV-GC/cmake-build-debug/bin/ycsbc"
+  YCSB_BIN="$HOME/ParaKV-GC/build/bin/ycsbc"
 else
-  YCSB_BIN="$HOME/ParaKV/cmake-build-debug/bin/ycsbc"
+  YCSB_BIN="$HOME/ParaKV/build/bin/ycsbc"
 fi
 
 ##########################
